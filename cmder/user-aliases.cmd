72ce52817232d038818cf947d61b7b0eab59d6dc
Michell Ocaña <michell@wplay.com.br>
2017-07-19 14:30:28 -0300

    Initial commit

---
+++ cmder/user-aliases.cmd
@@ -0,0 +1,16 @@
+@echo off
+rem To let cmder know your aliases, put this file in "PATH/TO/CMDER/config".
+
+rem Git
+gst=git status
+gcm=git commit
+gpl=git pull
+gps=git push
+gadal=git add --all
+gd=git diff $
+gl=log --pretty=oneline -n 20 --graph --abbrev-commit
+
+rem Ionic
+is=ionic serve
+iba=ionic build android
+ira=ionic run android
