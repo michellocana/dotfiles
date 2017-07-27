b6cea2365afc9099b1b5fa40d2d566aed4b31026
Michell Ocaña <michell@wplay.com.br>
2017-07-27 13:23:37 -0300

    Comment fix in cmder user aliases

--- cmder/user-aliases.cmd
+++ cmder/user-aliases.cmd
@@ -1,7 +1,7 @@
 @echo off
-rem To let cmder know your aliases, put this file in "PATH/TO/CMDER/config".
+;= rem To let cmder know your aliases, put this file in "PATH/TO/CMDER/config".
 
-rem Git
+;= rem Git
 gst=git status
 gcm=git commit
 gpl=git pull
@@ -10,7 +10,7 @@ gadal=git add --all
 gd=git diff $
 gl=log --pretty=oneline -n 20 --graph --abbrev-commit
 
-rem Ionic
+;= rem Ionic
 is=ionic serve
 iba=ionic build android
 ira=ionic run android
